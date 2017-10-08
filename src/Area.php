f15b5d9e3830003258149407a8fb986b8e6dd24d
Sunel Tr <sunelbe@gmail.com>
2017-10-08 20:55:35 +0530

    Added area set based on .env file

--- src/Area.php
+++ src/Area.php
@@ -12,6 +12,14 @@ class Area implements AreaResolver
      */
     public function owns(Application $app)
     {
+        
+        // If Area is set using .env file
+        
+        if (env('APP_AREA')) {
+            $this->setApplicationArea($app);
+            return true;
+        }
+
         // We will not be able to detect the area until the router is matched.
         // So we add a Illuminate\Routing\Events\RouteMatched listener and
         // when the router is matched we set the area for the application
@@ -19,13 +27,19 @@ class Area implements AreaResolver
 
         $app['events']->listen('Illuminate\Routing\Events\RouteMatched', function ($event) use ($app) {
             if ($app['router']->is('backend.*')) {
-                $app['area'] = 'backend';
-                $app->singleton(
-                    \Illuminate\Contracts\Debug\ExceptionHandler::class,
-                    \Mods\Backend\Exceptions\Handler::class
-                );
+                $this->setApplicationArea($app);
             }
         });
         return false;
+    }
+
+
+    protected function setApplicationArea(Application $app)
+    {
+        $app['area'] = 'backend';
+        $app->singleton(
+            \Illuminate\Contracts\Debug\ExceptionHandler::class,
+            \Mods\Backend\Exceptions\Handler::class
+        );
     }
 }
