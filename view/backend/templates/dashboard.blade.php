341c18add1243d769bddf6bba97a5f3bfb621479
Sunel Tr <sunelbe@gmail.com>
2017-10-22 13:35:35 +0530

    added emoty block to layout

---
+++ view/backend/templates/dashboard.blade.php
@@ -0,0 +1,14 @@
+<div class="col s12 m6 l3">
+    <div class="card">
+        <div class="card-content green white-text">
+            <p class="card-stats-title"><i class="mdi-social-group-add"></i> New Clients</p>
+            <h4 class="card-stats-number">566</h4>
+            <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-up"></i> 15% <span class="green-text text-lighten-5">from yesterday</span> </p>
+        </div>
+        <div class="card-action green darken-2">
+            <div id="clients-bar" class="center-align">
+                <canvas width="227" height="25" style="display: inline-block; width: 227px; height: 25px; vertical-align: top;"></canvas>
+            </div>
+        </div>
+    </div>
+</div>
